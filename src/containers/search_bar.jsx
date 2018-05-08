a8fb5c4b2ca8ada3f7ad22ced9bc4a9d29936de6
Rafael Herculano <rafa.lj@hotmail.com>
2018-05-07 21:19:16 -0300

    Adicionando a search bar

---
+++ src/containers/search_bar.jsx
@@ -0,0 +1,13 @@
+import React from 'react' ; 
+
+export default class SearchBar extends React.Component {
+
+	render(){
+		return <form className="input-group">
+			<input />
+			<span className="input-group-btn">
+				<button type="submit" className="btn btn-secondary">Buscar</button>
+			</span>
+		</form>
+	}
+}
