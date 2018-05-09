6010160f1f156511e6d85693f9fa5d58124c1e8b
Rafael Herculano <rafa.lj@hotmail.com>
2018-05-09 07:53:46 -0300

    point

--- src/containers/search_bar.jsx
+++ src/containers/search_bar.jsx
@@ -2,9 +2,28 @@ import React from 'react' ;
 
 export default class SearchBar extends React.Component {
 
+	constructor(props){
+		super(props)
+
+		this.state = {term: ''}
+
+		//bind
+		this.onInputChange = this.onInputChange.bind(this) ;
+	}
+	
+	onInputChange(event){
+		this.setState({term: event.target.value})
+		console.log(event.target.value) ;
+	}
+
 	render(){
 		return <form className="input-group">
-			<input />
+			<input
+				onChange = {this.onInputChange}
+				className='form-control' 
+				placeholder = "Selecione a Cidade"
+				value={this.state.term}
+			 />
 			<span className="input-group-btn">
 				<button type="submit" className="btn btn-secondary">Buscar</button>
 			</span>
