e71153f84f3fd8f0670e9c4435b39a8dabf3bd1d
Rafael Herculano <rafael.martins@psbi.com.br>
2018-05-10 10:52:04 -0300

    Configurando a action fetchWeather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios' ;
+
 const API_KEY = '35e797356b8be88dc6fe065813269cf9' ;
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ; 
+
+export const FETCH_WEATHER = 'FETCH_WEATHER' ;
+
+export function fetchWeather(city){
+	
+	const URL = `${ROOT_URL}&q=${city},us` ;
+	
+	const request = axios.get(URL) ;
+
+	return {
+		type: FETCH_WEATHER, 
+		payload: request 
+	}
+}
