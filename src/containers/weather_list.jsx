678b639d96fc377398edff1b1c2e85d655e2194f
Rafael Herculano <rafael.martins@psbi.com.br>
2018-05-11 12:00:03 -0300

    Criando container WeatherList e a conexao deste com o reducer de weather

---
+++ src/containers/weather_list.jsx
@@ -0,0 +1,31 @@
+import React from 'react' ; 
+import { connect } from 'react-redux' ;
+
+class WeatherList extends React.Component{
+	render(){
+		return(
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>Cidade</th>
+						<th>Temperatura</th>
+						<th>Pressão</th>
+						<th>Umidade</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+				</tbody>
+			</table>
+
+
+
+		)
+	}
+}
+
+function mapStateToProps(state){
+	return { weather: state.weather } ;
+}
+
+export default connect(mapStateToProps)(WeatherList) ;
